49e30f22c64783a1835ce4749bd05893b0b69058
Hugh Van <hvan021@gmail.com>
2014-05-22 16:37:21 +1200

    tut 4

--- polls/urls.py
+++ polls/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import patterns, url
 
 from polls import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns('polls.views',
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', 'index', name='index'),
     # ex: /polls/5/
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail', name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results', name='results'),
     # ex: /polls/5/vote/
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote', name='vote'),
 )
 
--- project1/urls.py
+++ project1/urls.py
@@ -5,9 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'project1.views.home', name='home'),
+    # url(r'^$', 'project1.views.index', name='home'),
+    # url(r'^$', 'project1.')
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
 )
